b32d1ae43e51d3829445fffd5863ccfea4cceef6
joaohall <enderfiremite@gmail.com>
2025-10-08 08:33:44 -0300

    .

--- api/index.py
+++ api/index.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
-from mangum import Mangum
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/api/")
+@app.get("/")
 def root():
     return {"message": "API root"}
 
-@app.get("/api/hello")
+@app.get("/hello")
 def hello():
     return {"message": "Hello from FastAPI"}
 
-handler = Mangum(app)
+# IMPORTANTE: A Vercel precisa deste handler
+def handler(event, context):
+    # Importa o Mangum apenas quando necessário
+    from mangum import Mangum
+    asgi_handler = Mangum(app)
+    return asgi_handler(event, context)
