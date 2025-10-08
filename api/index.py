3140ec7e3816868a8033895a088981c8777a06de
joaohall <enderfiremite@gmail.com>
2025-10-08 08:13:48 -0300

    .

--- api/index.py
+++ api/index.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api/hello")
+@app.get("/")  # Acessa via /api/index
+def root():
+    return {"message": "API root"}
+
+@app.get("/hello")  # Acessa via /api/index/hello
 def hello():
     return {"message": "Hello from FastAPI"}
